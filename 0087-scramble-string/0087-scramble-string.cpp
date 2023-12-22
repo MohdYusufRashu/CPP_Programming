713a3929017cb52281a4d996d9dd55094a9a11e4
MohdYusufRashu <62906034+MohdYusufRashu@users.noreply.github.com>
2023-12-22 14:43:28 +0530

    Time: 222 ms (9.31%), Space: 45.3 MB (8.66%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    //vector<vector<vector<string>>> dp;
     unordered_map<string,bool> dp;
     bool helper(string s1,string s2){
         string temp=s1 + "#" + s2;
@@ -16,26 +15,23 @@ public:
         if(s1.size()==1){
             dp[temp]=false;
             return false;
-        } ;
+        } 
         string x,y;
-        bool ans=false;
         for(int i=0;i<s1.size()-1;i++){
             x=s1.substr(0,i+1);
             y=s1.substr(i+1);
             int xl=x.size();
             int yl=y.size();
-            bool temp=false;
-            temp=temp || (helper(x,s2.substr(0,xl)) && helper(y,s2.substr(xl)));
-            temp = temp || (helper(y,s2.substr(0,yl)) && helper(x,s2.substr(yl,xl)));
-            ans=ans || temp; 
-            if(ans) break;
+            if((helper(x,s2.substr(0,xl)) && helper(y,s2.substr(xl))) || (helper(y,s2.substr(0,yl)) && helper(x,s2.substr(yl,xl)))){
+                dp[temp]=true;
+                return true;
+            }
         }
-        dp[temp]=ans;
-        return ans;
+        dp[temp]=false;
+        return false;
     }
     bool isScramble(string s1, string s2) {
         int n=s1.size();
-       // dp=vector<vector<vector<int>>>(n,vector<vector<int>>(n,vector<int>(n,-1)));
         return helper(s1,s2);
     }
 };
