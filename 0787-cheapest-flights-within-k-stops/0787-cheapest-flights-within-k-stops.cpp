aab7a1c096543413bf74021eb1047a981e2061d7
MohdYusufRashu <62906034+MohdYusufRashu@users.noreply.github.com>
2023-12-22 19:17:27 +0530

    Time: 59 ms (10.49%), Space: 13.5 MB (69.94%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> weights(n,INT_MAX);
+        weights[src]=0;
+        vector<vector<pair<int,int>>> graph(n);
+        for(int i=0;i<flights.size();i++){
+            graph[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        }
+        for(int x=0;x<=k;x++){
+            vector<int> temp=weights;
+            for(int i=0;i<n;i++){
+                int v1=i;
+                for(int j=0;j<graph[v1].size();j++){
+                    int v2=graph[i][j].first;
+                    int cost=graph[i][j].second;
+                    if(temp[v1]!=INT_MAX && (weights[v2]>(temp[v1]+cost))){
+                        weights[v2]=temp[v1]+cost;
+                    }
+                    /*if(v2==dst){
+                        cout<<weights[v2]<<endl;
+                    }*/
+                }
+            }
+            for(int i=0;i<n;i++){
+                cout<<weights[i]<<" ";
+            }
+            cout<<endl;
+        }
+        if(weights[dst]==INT_MAX) return -1;
+        return weights[dst];
+    }
+};
