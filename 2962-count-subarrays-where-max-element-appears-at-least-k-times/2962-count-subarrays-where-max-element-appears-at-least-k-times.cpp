446cdbbb7297149e15baa94995c9f962bc0aa616
MohdYusufRashu <62906034+MohdYusufRashu@users.noreply.github.com>
2024-03-29 14:51:29 +0530

    Time: 281 ms (13.18%), Space: 149.7 MB (15.31%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -5,12 +5,10 @@ public:
         int n=nums.size();
         long long ans=0;
         int maxVal=*max_element(nums.begin(),nums.end());
-        //cout<<"maxVal : "<<maxVal<<endl;
         unordered_map<int,int> map;
         while(j<n){
             map[nums[j]]++;
             while(map[maxVal]==k){
-                //cout<<n-j<<endl;
                 ans+=n-j;
                 map[nums[i]]--;
                 i++;
